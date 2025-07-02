a543268bd23a585b0c2d9989e092094aa3927c75
kavinda1996 <gayashankavinda95@gmail.com>
2025-07-02 12:08:36 +0530

    Adding Home button

--- 113-Customer-Manage-Back-End/src/main/java/edu/icet/entity/CustomerEntity.java
+++ 113-Customer-Manage-Back-End/src/main/java/edu/icet/entity/CustomerEntity.java
@@ -1,20 +1,4 @@
 package edu.icet.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
-
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@ToString
-@Entity
-@Table(name = "customer")
 public class CustomerEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
-    private String name;
-    private String address;
-    private Double salary;
 }
--- 113-Customer-Manage-Back-End/src/main/java/edu/icet/repository/CustomerRepository.java
+++ 113-Customer-Manage-Back-End/src/main/java/edu/icet/repository/CustomerRepository.java
@@ -1,13 +1,4 @@
 package edu.icet.repository;
 
-import edu.icet.dto.Customer;
-import edu.icet.entity.CustomerEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-
-import java.util.List;
-
-public interface CustomerRepository extends JpaRepository<CustomerEntity,Integer> {
-    List<CustomerEntity> findByName(String name);
-    List<CustomerEntity> findByAddress(String address);
+public interface CustomerRepository {
 }
