321f652c17223a18c2f9589cccc693bdd9425875
kavinda1996 <gayashankavinda95@gmail.com>
2025-07-04 01:16:28 +0530

    Update CustomerRepository.java

--- 113-Customer-Manage-Back-End/src/main/java/edu/icet/repository/CustomerRepository.java
+++ 113-Customer-Manage-Back-End/src/main/java/edu/icet/repository/CustomerRepository.java
@@ -1,4 +1,11 @@
 package edu.icet.repository;
 
-public interface CustomerRepository {
+import edu.icet.entity.CustomerEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CustomerRepository extends JpaRepository<CustomerEntity,Integer> {
+    List<CustomerEntity> findByName(String name);
+    List<CustomerEntity> findByAddress(String address);
 }
