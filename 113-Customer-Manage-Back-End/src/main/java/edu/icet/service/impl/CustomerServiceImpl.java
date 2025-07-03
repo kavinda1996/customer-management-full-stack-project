6097f6bdad223874b649662bf7d0ec50f916466d
kavinda1996 <gayashankavinda95@gmail.com>
2025-07-04 01:17:17 +0530

    Update CustomerServiceImpl.java

--- 113-Customer-Manage-Back-End/src/main/java/edu/icet/service/impl/CustomerServiceImpl.java
+++ 113-Customer-Manage-Back-End/src/main/java/edu/icet/service/impl/CustomerServiceImpl.java
@@ -1,4 +1,69 @@
 package edu.icet.service.impl;
 
-public class CustomerServiceImpl {
+import edu.icet.dto.Customer;
+import edu.icet.entity.CustomerEntity;
+import edu.icet.repository.CustomerRepository;
+import edu.icet.service.CustomerService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerServiceImpl implements CustomerService {
+
+    final CustomerRepository repository;
+    final ModelMapper mapper;
+
+    @Override
+    public void addCustomer(Customer customer) {
+        repository.save(mapper.map(customer, CustomerEntity.class));
+    }
+
+    @Override
+    public List<Customer> getAll() {
+        List<Customer> customerList = new ArrayList<>();
+        List<CustomerEntity> all = repository.findAll();
+
+        all.forEach(customerEntity -> {
+            customerList.add(mapper.map(customerEntity, Customer.class));
+        });
+
+        return customerList;
+    }
+
+    @Override
+    public void deleteCustomer(Integer id) {
+        repository.deleteById(id);
+    }
+
+    @Override
+    public void updateCustomer(Customer customer) {
+        repository.save(mapper.map(customer, CustomerEntity.class));
+    }
+
+    @Override
+    public Customer searchById(Integer id) {
+        return mapper.map(repository.findById(id), Customer.class);
+    }
+
+    @Override
+    public List<Customer> searchByName(String name) {
+        List<CustomerEntity> byName = repository.findByName(name);
+        List<Customer> customerList = new ArrayList<>();
+
+        byName.forEach(customerEntity -> {
+            customerList.add(mapper.map(customerEntity, Customer.class));
+        });
+        return customerList;
+    }
 }
+//    @Override
+//    public int count (int a, int b){
+//        return a+b;
+//    }
+//
+
