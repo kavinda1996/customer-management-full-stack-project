4358b3bdf6dd71a1ccbecec49d64157b98e97bc6
kavinda1996 <gayashankavinda95@gmail.com>
2025-07-04 01:17:50 +0530

    Update CustomerService.java

--- 113-Customer-Manage-Back-End/src/main/java/edu/icet/service/CustomerService.java
+++ 113-Customer-Manage-Back-End/src/main/java/edu/icet/service/CustomerService.java
@@ -1,4 +1,21 @@
 package edu.icet.service;
 
+import edu.icet.dto.Customer;
+
+import java.util.List;
+
 public interface CustomerService {
+    void addCustomer(Customer customer);
+
+    List<Customer> getAll();
+
+    void deleteCustomer(Integer id);
+
+    void updateCustomer(Customer customer);
+
+    Customer searchById(Integer id);
+
+    List<Customer> searchByName(String name);
+
+//    int count (int a,int b);
 }
